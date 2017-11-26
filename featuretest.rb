440ab7eb2a09ac1f57030735476518c6f24704ec
kkumshayev <kumshkesh@gmail.com>
2017-11-26 23:39:16 +0000

    failing feature test for takeoff during storm

--- featuretest.rb
+++ featuretest.rb
@@ -6,8 +6,17 @@ airport = Airport.new
 
 # p plane.land(airport)
 
-# plane takes off
-p plane.takeoff
+# # plane takes off
+# p plane.takeoff
+#
+# # expecting false (plane no longer docked at airport)
+# p airport.planes_docked.include?(plane)
 
-# expecting false (plane no longer docked at airport)
-p airport.planes_docked.include?(plane)
+p plane.land(airport)
+p 'the weather at the airport is'
+p airport.weather
+p 'a storm is coming'
+p airport.weather = :stormy
+p 'takeoff not permitted'
+# takeoff should now raise an error
+p plane.takeoff
