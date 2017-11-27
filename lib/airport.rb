83a3feae3abc869f4d56aa6cde03360dcad40380
kkumshayev <kumshkesh@gmail.com>
2017-11-27 00:53:16 +0000

    failing unit test for airport capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -12,13 +12,14 @@ class Airport
 
   def request_landing(plane)
     raise 'Planes cannot land during a storm.' if @weather == :stormy
-
+    raise 'Cannot land. The airport is full.' if @planes_docked.count >= @capacity
+    @planes_docked << plane
     self
   end
 
   def request_takeoff(plane)
     raise 'Planes cannot take off during a storm.' if @weather == :stormy
-
+    @planes_docked.pop
     self
   end
 
